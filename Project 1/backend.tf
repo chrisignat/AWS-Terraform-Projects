4867ce3e9c3bef51fbd50df505f9fffa5f60cbcb
Chris Ignatiadis <43823317+chrisignat@users.noreply.github.com>
2026-06-29 16:09:54 +0300

    Update backend.tf

--- Project 1/backend.tf
+++ Project 1/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "project-1-211125536240-us-east-1-an"
-    key          = "project3/vpc/terraform.tfstate"
+    key          = "project1/vpc/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true 
